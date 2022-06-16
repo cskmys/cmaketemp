ac7ec670314b737ffe659a623cf51c909711082a
csk <karthik-subramanyam.chakka@grenoble-inp.org>
2022-06-17 01:26:16 +0200

    creating a simple workspace

---
+++ src/prj1/subprj2/hello.cpp
@@ -0,0 +1,7 @@
+//
+// Created by csk on 16/06/2022.
+//
+#include<iostream>
+int main(){
+    std::cout<<"Hello from prj1 subprj2"<<std::endl;
+}
---
+++ src/prj2/subprj1/hello.cpp
@@ -0,0 +1,7 @@
+//
+// Created by csk on 16/06/2022.
+//
+#include<iostream>
+int main(){
+    std::cout<<"Hello from prj2 subprj1"<<std::endl;
+}
